ebb1236b099fc587b30062c9b2aa6bd80d81d3cb
janispi <janis.pirsko@gmail.com>
2021-09-08 22:04:29 +0300

    pirmais uzd

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sveiks, kā Tevi sauc?");
+
+            string userName = Console.ReadLine();
+            Console.WriteLine("Labdien, " + userName);
+        }
+    }
+}
